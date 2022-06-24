a6a2f4fbcd1de77cb09684d0f0d57ecdb01dbd6c
Chukwuka Morka <c.morka@capitalsage.ng>
2022-06-24 15:37:30 +0100

    added cart context and shopping icon as well as cart dropdown

--- crown-clothing-ecommerce-app/src/App.js
+++ crown-clothing-ecommerce-app/src/App.js
@@ -2,7 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import {Home} from "./routes/home/home.component";
 import {Navigation} from "./routes/navigation/navigation.component";
 import {Authentication} from "./routes/authentication/authentication.component";
-
+import {Shop} from "./routes/shop/shop.component"
 
 export const App = () => {
 
@@ -10,6 +10,7 @@ export const App = () => {
         <Route path="/" element={<Navigation/>}>
             <Route index element={<Home/>}/>
             <Route path="auth" element={<Authentication/>}/>
+            <Route path="shop" element={<Shop/>}/>
         </Route>
     </Routes>)
 }
